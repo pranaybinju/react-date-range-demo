f5e18bb04722e14a51ddbd64bb7805e3b4d76525
PKB2810 <pranay@kiprosh.com>
2020-07-06 19:41:01 +0530

    basic date range

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { addDays } from "date-fns";
+import "./App.css";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import {
+  createStaticRanges,
+  DateRange,
+  DefinedRange,
+  Calendar,
+} from "react-date-range";
 
+const renderStaticRangeLabel = () => <StaticRenderingLabel />;
+
+const StaticRenderingLabel = (props) => {
+  // const labels = [
+  //   "Today",
+  //   "This Month",
+  //   "Last Month",
+  //   "Till Today as of month",
+  //   "This Year",
+  //   "Last Year",
+  //   "All time",
+  //   "Custom Range",
+  // ].map((elem) => );
+
+  return <span>{"Holla"}</span>;
+};
 function App() {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+
+  const predefinedRanges = createStaticRanges([
+    {
+      label: "Custom Range",
+      range: () => ({
+        startDate: new Date(),
+        endDate: new Date(),
+      }),
+      isSelected: () => {
+        console.log("selected");
+        return true;
+      },
+    },
+    {
+      label: "Today",
+      range: () => ({
+        startDate: new Date(),
+        endDate: new Date(),
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+    {
+      label: "This Month",
+      range: () => ({
+        startDate: null,
+        endDate: null,
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+    {
+      label: "Last Month",
+      range: () => ({
+        startDate: null,
+        endDate: null,
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+
+    {
+      label: "This Year",
+      range: () => ({
+        startDate: null,
+        endDate: null,
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+    {
+      label: "Last Year",
+      range: () => ({
+        startDate: null,
+        endDate: null,
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+    {
+      label: "All time",
+      range: () => ({
+        startDate: null,
+        endDate: null,
+      }),
+      isSelected() {
+        return true;
+      },
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <DefinedRange
+        renderStaticRangeLabel={renderStaticRangeLabel}
+        staticRanges={predefinedRanges}
+        inputRanges={[]}
+        onChange={(item) => setState([item.selection])}
+      />
+      <DateRange
+        onChange={(item) => setState([item.selection])}
+        showSelectionPreview={true}
+        months={2}
+        ranges={state}
+        direction="horizontal"
+      />
     </div>
   );
 }
